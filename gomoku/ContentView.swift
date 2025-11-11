5295afdaa68ff640efc5bed953375debc455762e
Li Zheng <li_zheng@outlook.com>
2025-11-11 11:22:21 -0500

    Initial Commit

---
+++ gomoku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gomoku
+//
+//  Created by Li Zheng on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gomoku/gomokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gomokuApp.swift
+//  gomoku
+//
+//  Created by Li Zheng on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct gomokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
